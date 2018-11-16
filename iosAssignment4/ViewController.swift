594e61d1ef200188058b34228c4a9311c4e9d32c
guer0157 <32390693+guer0157@users.noreply.github.com>
2018-11-16 15:41:28 -0500

    Initial Commit

---
+++ iosAssignment4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosAssignment4
+//
+//  Created by Cesar Guerrero on 2018-11-16.
+//  Copyright © 2018 guer0157.AlgonquinCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
